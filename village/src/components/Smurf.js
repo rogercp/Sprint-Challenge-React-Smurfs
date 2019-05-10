179760112a605b871164f72f74dca5e142bab3e8
Roger Caballero <rogercp1234@yahoo.com>
2019-05-10 10:57:48 -0700

    cleaned up code

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -3,26 +3,27 @@ import React from 'react';
 
 
 
-class Smurf extends React.Component {
+const Smurf =props=> {
 
-  onDelete =id=>{
-    console.log(id)
-      this.props.deleteSmurf(id);
+  // onDelete =id=>{
+  //   console.log(id)
+  //     this.props.deleteSmurf(id);
 
-  }
+  // }
 
-
-  render(){
+ 
   return (
     <div className="Smurf" style={{padding:"20px"}}>
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
-      <button onClick={()=>this.onDelete(this.props.id)}>delete</button>
+      <button 
+      // onClick={()=>this.onDelete(this.props.id)}
+      >delete</button>
     </div>
   );
 };
-}
+
 
 Smurf.defaultProps = {
   name: '',
